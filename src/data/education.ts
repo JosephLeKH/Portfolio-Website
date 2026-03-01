60ed73e2742f5fc6539d1fbd84828a70f77e5e0c
Joseph Le <99309085+JosephLeKH@users.noreply.github.com>
2026-02-28 18:53:45 -0800

    Update education courses by removing 'CS 155' and adding 'ENGR 76', and replace resume PDF with the latest version.

--- src/data/education.ts
+++ src/data/education.ts
@@ -21,12 +21,11 @@ export const awards = [
 
 export const courses = [
   { code: 'CS 221', fullName: 'Artificial Intelligence: Principles and Techniques', link: 'https://explorecourses.stanford.edu/search?q=CS+221' },
-  { code: 'CS 155', fullName: 'Computer and Network Security', link: 'https://explorecourses.stanford.edu/search?q=CS+155' },
   { code: 'CS 205L', fullName: 'Continuous Mathematical Methods with an Emphasis on Machine Learning', link: 'https://explorecourses.stanford.edu/search?q=CS+205L' },
   { code: 'CS 161', fullName: 'Design and Analysis of Algorithms', link: 'https://explorecourses.stanford.edu/search?q=CS+161' },
   { code: 'CS 124', fullName: 'From Languages to Information (NLP)', link: 'https://explorecourses.stanford.edu/search?q=CS+124' },
   { code: 'CS 111', fullName: 'Operating Systems Principles', link: 'https://explorecourses.stanford.edu/search?q=CS+111' },
-  { code: 'DATASCI 154', fullName: 'Data Science for Social Impact', link: 'https://explorecourses.stanford.edu/search?q=DATASCI+154' },
+  { code: 'ENGR 76', fullName: 'Information Science and Engineering', link: 'https://explorecourses.stanford.edu/search?q=ENGR+76' },
   { code: 'DATASCI 161', fullName: 'Causality, Decision Making and Data Science', link: 'https://explorecourses.stanford.edu/search?q=DATASCI+161' },
   { code: 'MATH 104', fullName: 'Applied Matrix Theory', link: 'https://explorecourses.stanford.edu/search?q=MATH+104' },
   { code: 'CS 107', fullName: 'Computer Organization and Systems', link: 'https://explorecourses.stanford.edu/search?q=CS+107' },
